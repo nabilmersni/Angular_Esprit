b99394438d0aef971355b3fb515dcc99800f0fb2
nabilmersni <nabilmersni123@gmail.com>
2023-09-30 17:22:39 +0100

    Atelier n°1 :
    Manipulation de composant (partie1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListProduitsComponent } from './list-produits/list-produits.component';
 import { FormsModule } from '@angular/forms';
+import { ListFournisseurComponent } from './list-fournisseur/list-fournisseur.component';
 
 @NgModule({
-  declarations: [AppComponent, ListProduitsComponent],
+  declarations: [AppComponent, ListProduitsComponent, ListFournisseurComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/list-fournisseur/list-fournisseur.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Fournisseur } from 'src/models/Fournisseur';
+
+@Component({
+  selector: 'app-list-fournisseur',
+  templateUrl: './list-fournisseur.component.html',
+  styleUrls: ['./list-fournisseur.component.scss'],
+})
+export class ListFournisseurComponent {
+  ////////////////// EX1
+  idFournisseur = 105;
+  code = 'A104B89';
+  libelle = 'MyTeck';
+  hide: boolean = false;
+
+  toggleFournisseur = () => {
+    this.hide = !this.hide;
+    console.log(this.hide);
+  };
+
+  ////////////////// EX2
+  fournisseurs: Fournisseur[] = [
+    { idFournisseur: 1, code: '1ABC4522', libelle: 'hp' },
+    { idFournisseur: 2, code: '2ABC4522', libelle: 'dell' },
+    { idFournisseur: 3, code: '3ABC4522', libelle: 'lenovo' },
+    { idFournisseur: 400, code: '40ABC452', libelle: 'asus' },
+  ];
+
+  test = (index: number) => {
+    console.log(index);
+    this.fournisseurs.splice(index, 1);
+  };
+}
